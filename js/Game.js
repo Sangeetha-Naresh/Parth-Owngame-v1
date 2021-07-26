9760192aea6a17b59bd3c6be4d5033b1b90a6787
Sangeetha-Naresh <73550579+Sangeetha-Naresh@users.noreply.github.com>
2021-07-26 12:53:51 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,118 @@
+class Game 
+{
+ getState()
+ {
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start()
+  {
+    if(gameState === 0)
+    {
+        player = new Player();
+        var playerCountRef = await database.ref('playerCount').once("value");
+        if(playerCountRef.exists()){
+          playerCount = playerCountRef.val();
+          player.getCount();
+        }
+        form = new Form()
+        form.display();
+    }
+
+    guard=createSprite(200,600);
+    guard.addAnimation("guard",guardimg);
+    guard.scale=0.7;
+
+    thief = createSprite(800,600);
+    thief.addAnimation("thief",thiefimg);
+    thief.scale=1.5;
+
+    players=[guard,thief];
+
+  }
+
+  play()
+  {
+
+    form.hide();
+
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined)
+    {
+      background(0);
+      image(bg,0,0, 1330*9,750);
+      var index = 0;
+      
+      for(var plr in allPlayers)
+      {
+        
+        index = index + 1 ;
+
+        x= index * 200 + allPlayers[plr].distance;
+        console.log(x);
+        y= allPlayers[plr].y;
+        //y=450;
+
+        players[index-1].x=x;
+        players[index-1].y=y;
+        
+        if (index === player.index)
+        {
+
+          camera.position.x = players[index-1].x ;
+          camera.position.y = y;
+
+          fill("red");
+          ellipse(x,y,80,80)
+        
+
+         
+
+      }
+
+      }
+
+      if(guard.isTouching(thief))
+      {
+        game.end();
+      }
+
+      
+
+    }
+
+    if(keyIsDown(RIGHT_ARROW) && player.index!==null)
+    {
+   
+      player.distance =player.distance+ 10;
+      
+      player.update();
+    }
+
+   
+
+
+    
+
+    drawSprites();
+  }
+
+  end()
+  {
+    alert("GAME OVER!!!")
+
+    gameState=0;
+  }
+  
+}
